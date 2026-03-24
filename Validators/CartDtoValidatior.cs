704b99086b311ddc87b800df750c5b7ad65efd14
DorogaSmerti <andrush.cuts@gmail.com>
2026-03-24 13:27:00 +0200

    fech: add CartDtoValidator and ChangeQuantityDtoValidator

---
+++ Validators/CartDtoValidatior.cs
@@ -0,0 +1,25 @@
+using FluentValidation;
+using MyFirstProject.Models;
+
+namespace MyFirstProject.Validators;
+
+public class CartDtoValidatior : AbstractValidator<AddToCartDto>
+{
+    public CartDtoValidatior()
+    {
+        RuleFor(x => x.ProductId)
+        .GreaterThan(0).WithMessage("ProductId must be greater than 0");
+
+        RuleFor(x => x.Quantity)
+        .GreaterThan(0).WithMessage("Quantity must be greater than 0");
+    }
+}
+
+public class ChangeQuantityDtoValidator : AbstractValidator<ChangeQuantityDto>
+{
+    public ChangeQuantityDtoValidator()
+    {
+        RuleFor(x => x.Quantity)
+        .GreaterThan(0).WithMessage("Quantity must be greater than 0");
+    }
+}
